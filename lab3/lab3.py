2de4375e2b728db2b208a0c0c552f972ca5e7720
Ujjwal <ujjwal.jain1012@gmail.com>
2021-03-10 23:59:00 -0800

    Updated output

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -16,10 +16,6 @@ mpl.rc('axes', labelsize=14)
 mpl.rc('xtick', labelsize=12)
 mpl.rc('ytick', labelsize=12)
 
-
-# In[106]:
-
-
 from sklearn.linear_model import SGDClassifier
 from sklearn.datasets import fetch_openml
 from sklearn.neighbors import KNeighborsClassifier
@@ -27,9 +23,6 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import confusion_matrix
-
-
-# In[107]:
 
 
 def random_digit():
@@ -41,16 +34,12 @@ def random_digit():
     plt.show()
 
 
-# In[108]:
-
 
 def load_data():
     mnist = fetch_openml('mnist_784', version=1, cache=True)
     mnist.target = mnist.target.astype(np.int8) # fetch_openml() returns targets as strings
     return mnist
 
-
-# In[109]:
 
 
 mnist = load_data()
@@ -59,13 +48,9 @@ y = mnist.target.to_numpy()
 X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.10, random_state=42)
 
 
-# In[110]:
-
 
 random_digit()
 
-
-# In[111]:
 
 
 def create_new_y(dataset):
@@ -81,7 +66,6 @@ new_y_train = create_new_y(y_train)
 new_y_test = create_new_y(y_test)
 
 
-# In[112]:
 
 
 def SGDModel(x,y):
@@ -99,55 +83,32 @@ def KNNmodel(x,y):
     return knn
 
 
-# In[113]:
-
-
 clf = SGDModel(X_train, new_y_train)
 SGDC_accuracy = getAccuracy(clf, X_test, new_y_test)
+print("SGDClassifier model accuracy is = ")
 print(SGDC_accuracy)
 
 
-# ### SGDClassifier Accuracy = 0.9671428571428572
-
-# In[114]:
+### SGDClassifier Accuracy = 0.9671428571428572
 
 
 knn = KNNmodel(X_train, new_y_train)
 KNN_accuracy = getAccuracy(knn, X_test, new_y_test)
+print("KNN Accuracy is = ")
 print(KNN_accuracy)
 
 
-# ### KNN Accuracy = 0.9952857142857143
-
-# In[125]:
-
+### KNN Accuracy = 0.9952857142857143
 
 def crossVal():
     y_test_pred = cross_val_predict(knn, X_test, new_y_test, cv=3)
     return confusion_matrix(new_y_test,y_test_pred)
 
 
-# In[127]:
-
-
 confusionMatrix = crossVal()
+print("Confusion Matrix = ")
 print(confusionMatrix)
-
-
-# In[115]:
-
 
 plt.imshow(X_test[1222].reshape(28,28))
 
-
-# In[116]:
-
-
 y_test[1222]
-
-
-# In[ ]:
-
-
-
-
